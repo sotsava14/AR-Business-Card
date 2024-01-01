507623421f7dee0cdabca9743d9fb0b2afedbfbd
Sotsava Skandhaa <bajisotsavaskandhaa@gmail.com>
2024-01-01 23:28:07 +0530

    Initial commit

---
+++ Assets/Scripts/Anim.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    public void openInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/tomcruise/?hl=en");
+    }
+        public void openLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/tom-cruise-06828540/");
+    }
+
+}
